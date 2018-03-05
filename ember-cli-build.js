ff56522dacc09976c6bb87be5f9631abc3807e72
nandodonaire <nandodonaire@gmail.com>
2018-03-05 10:15:26 -0500

    Initial commit

---
+++ ember-cli-build.js
@@ -0,0 +1,14 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    'ember-bootstrap': {
+      'bootstrapVersion': 3,
+      'importBootstrapFont': true,
+      'importBootstrapCSS': false
+    }
+  });
+  return app.toTree();
+};
