b5598f149b4eef8a02c345ac2556d812b3e18352
nandodonaire <nandodonaire@gmail.com>
2018-03-07 10:27:57 -0500

    Added UI messaging when creating, updating, and deleting breweries
    --Added flashMessages (for both success and failure) when a user adds a
    new brewery, updates a brewery, and deletes a brewery.

--- app/routes/breweries/add-brewery.js
+++ app/routes/breweries/add-brewery.js
@@ -1,15 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  flashMessages: service(),
   model () {
-  return this.get('store').createRecord('brewery', {});
-},
-actions: {
-save(newBrewery) {
-  // console.log('Save ran!')
-      // console.log('Brewery name is', newBrewery.get('name'))
-      return newBrewery.save()
-        .then(()=> this.transitionTo('breweries'))
-}
-}
+    return this.get('store').createRecord('brewery', {});
+  },
+  actions: {
+  save(newBrewery) {
+    // console.log('Save ran!')
+        // console.log('Brewery name is', newBrewery.get('name'))
+        return newBrewery.save()
+          .then(()=> this.transitionTo('breweries'))
+          .then(() => this.get('flashMessages').success('New brewery has been saved!'))
+          .catch(() => {
+            this.get('flashMessages')
+            .danger('There was a problem saving the new brewery. Please try again.');
+          });
+        }
+      }
 });
--- app/routes/brewery.js
+++ app/routes/brewery.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  flashMessages: service(),
   model(params) {
     const id = +params.brewery_id
     return this.get('store').findRecord('brewery', id);
@@ -8,11 +10,22 @@ export default Route.extend({
   actions: {
     edit(model) {
       return model.save()
+        .then(()=> this.transitionTo('brewery'))
+        .then(() => this.get('flashMessages').success('Updates have been saved!'))
+        .catch(() => {
+          this.get('flashMessages')
+          .danger('There was a problem saving the updates. Please try again.');
+        });
     },
     deleteBrewery(model) {
       // console.log('Delete is triggering!')
       return model.destroyRecord()
         .then(()=> this.transitionTo('breweries'))
+        .then(() => this.get('flashMessages').success('Brewery has been deleted!'))
+        .catch(() => {
+          this.get('flashMessages')
+          .danger('There was a problem deleting the brewery. Please try again.');
+        });
   }
 }
 });
