e8bbef764c4bd01dab844f5b06c1b002727272fe
nandodonaire <nandodonaire@gmail.com>
2018-03-08 11:33:24 -0500

    Changed breweries route to only display saved breweries
    --Added a filterBy to the model hook to filter by 'isNew, false'. This
    makes it so that only saved breweries are displayed in the 'breweries'
    route.

--- app/routes/breweries.js
+++ app/routes/breweries.js
@@ -2,6 +2,11 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model () {
-    return this.get('store').findAll('brewery');
-  }
+    // return this.get('store').findAll('brewery');
+    return Ember.RSVP.hash({
+      breweries : this.get('store').findAll('brewery').then(function (result) {
+        return result.filterBy('isNew', false);
+      })
+  })
+}
 });
